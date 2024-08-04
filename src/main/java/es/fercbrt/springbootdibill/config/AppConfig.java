f1747fcdd1db14b71c4829a2549a4f13882c5f2c
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-04 17:04:23 +0200

    Created a @Bean for a list of items

--- src/main/java/es/fercbrt/springbootdibill/config/AppConfig.java
+++ src/main/java/es/fercbrt/springbootdibill/config/AppConfig.java
@@ -1,10 +1,22 @@
 package es.fercbrt.springbootdibill.config;
 
+import es.fercbrt.springbootdibill.models.Item;
+import es.fercbrt.springbootdibill.models.Product;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import java.util.List;
 
 @Configuration
 @PropertySource("classpath:data.properties")
 public class AppConfig {
-
+    @Bean
+    List<Item> billItems() {
+        return List.of(
+                new Item(new Product("Laptop", 30.0), 12),
+                new Item(new Product("Mouse", 10.0), 30),
+                new Item(new Product("Keyboard", 20.0), 20)
+        );
+    }
 }
