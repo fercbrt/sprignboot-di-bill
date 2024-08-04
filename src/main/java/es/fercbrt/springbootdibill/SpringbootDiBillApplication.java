35837fc2497e92d5e77cf8adc026766315a7b279
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-04 16:52:08 +0200

    Initial commit

---
+++ src/main/java/es/fercbrt/springbootdibill/SpringbootDiBillApplication.java
@@ -0,0 +1,13 @@
+package es.fercbrt.springbootdibill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDiBillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDiBillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/fercbrt/springbootdibill/SpringbootDiBillApplicationTests.java
@@ -0,0 +1,13 @@
+package es.fercbrt.springbootdibill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDiBillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
